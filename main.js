2ce544c2e38432ad1c1faf96977b6b2668dcf498
Egor Fedorov <fedorovegor33@gmail.com>
2015-03-31 15:10:49 +0300

    Fix resize on scroll issue! now resize is only triggered by width changes

--- main.js
+++ main.js
@@ -6,6 +6,30 @@ $(document).ready(function () {
     $("#lat_value").append("30");
     $("#height_value").append("20");
 
+    //maintain size chart ratio
+
+    var current_width = $(window).width();
+    var current_height = $(window).height();
+
+    function drawSatChart(sd) {
+        var canv = $("#sat_chart_canvas");
+        canv.css("width", "99%");
+        canv.css("margin", "1%");
+        var ctx = canv.get(0).getContext("2d");
+        ctx.canvas.width = window.innerWidth;
+        ctx.canvas.height = 0.5 * window.innerWidth;
+        new Chart(ctx).Bar(sd);
+    }
+
+    //delay to do autoresize less often
+    var delay = (function () {
+        var timer = 0;
+        return function (callback, ms) {
+            clearTimeout(timer);
+            timer = setTimeout(callback, ms);
+        };
+    })();
+
     var sat_data = {
         labels: ["January", "February", "March", "April", "May", "June", "July"],
         datasets: [
@@ -20,19 +44,27 @@ $(document).ready(function () {
         ]
     };
 
-    function drawSatChart(sd) {
-        var ctx = $("#sat_chart").get(0).getContext("2d");
-        ctx.canvas.width = window.innerWidth;
-        ctx.canvas.height = window.innerHeight;
-        var sat_bar_chart = new Chart(ctx).Bar(sd);
-    }
-
     drawSatChart(sat_data);
 
-
-    // take care of window resize
+    //take care of window resize
     $(window).resize(function () {
-        drawSatChart(sat_data);
+
+        var new_height = $(window).height(), new_width = $(window).width();
+
+        if (new_height != current_height) {
+            $("#fix_value").append("new_size");
+            current_height = new_height;
+        }
+
+        if (new_width != current_width) {
+            current_width = new_width;
+            delay(function () {
+                //alert('Resize...');
+                drawSatChart(sat_data);
+                $("#mode_value").append("resize");
+            }, 500);
+        }
+
     });
 
 });
