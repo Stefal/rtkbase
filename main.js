4b83b601e9fb7eb4a9cc258c60986246ae3262d8
Egor Fedorov <fedorovegor33@gmail.com>
2015-03-31 15:42:04 +0300

    two charts

--- main.js
+++ main.js
@@ -10,15 +10,16 @@ $(document).ready(function () {
 
     var current_width = $(window).width();
     var current_height = $(window).height();
+    var canvas1 = $("#sat_chart1_canvas");
+    var canvas2 = $("#sat_chart2_canvas");
 
-    function drawSatChart(sd) {
-        var canv = $("#sat_chart_canvas");
-        canv.css("width", "99%");
-        canv.css("margin", "1%");
-        var ctx = canv.get(0).getContext("2d");
+    function drawSatChart(canvas, sat_data) {
+        canvas.css("width", "99%");
+        canvas.css("margin", "1%");
+        var ctx = canvas.get(0).getContext("2d");
         ctx.canvas.width = window.innerWidth;
         ctx.canvas.height = 0.5 * window.innerWidth;
-        new Chart(ctx).Bar(sd);
+        new Chart(ctx).Bar(sat_data);
     }
 
     //delay to do autoresize less often
@@ -30,21 +31,40 @@ $(document).ready(function () {
         };
     })();
 
-    var sat_data = {
+    function updateSatCharts() {
+        drawSatChart(canvas1, sat_data1);
+        drawSatChart(canvas2, sat_data2);
+    }
+
+    var sat_data1 = {
         labels: ["January", "February", "March", "April", "May", "June", "July"],
         datasets: [
             {
                 label: "My First dataset",
-                fillColor: "rgba(0,255,00,0.9)",
-                strokeColor: "rgba(220,220,220,0.8)",
-                highlightFill: "rgba(220,220,220,0.75)",
-                highlightStroke: "rgba(220,220,220,1)",
+                fillColor: "rgba(0, 255, 0, 0.9)",
+                strokeColor: "rgba(220, 220, 220, 0.8)",
+                highlightFill: "rgba(220, 220, 220, 0.75)",
+                highlightStroke: "rgba(220, 220, 220, 1)",
                 data: [65, 59, 80, 81, 56, 55, 40]
             }
         ]
     };
 
-    drawSatChart(sat_data);
+    var sat_data2 = {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [
+            {
+                label: "My First dataset",
+                fillColor: "rgba(255, 0, 0, 0.9)",
+                strokeColor: "rgba(220, 220, 220, 0.8)",
+                highlightFill: "rgba(220, 220, 220, 0.75)",
+                highlightStroke: "rgba(220, 220, 220, 1)",
+                data: [65, 59, 80, 81, 56, 55, 40]
+            }
+        ]
+    };
+
+    updateSatCharts();
 
     //take care of window resize
     $(window).resize(function () {
@@ -59,7 +79,7 @@ $(document).ready(function () {
             current_width = new_width;
             delay(function () {
                 //alert('Resize...');
-                drawSatChart(sat_data);
+                updateSatCharts();
             }, 500);
         }
 
