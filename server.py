2c4060827391fec23c4d99134b479b788d36503e
Egor Fedorov <fedorovegor33@gmail.com>
2015-04-17 20:05:20 +0300

    Server now emits satellite level messages Client parses them and updates the chart correspondingly

--- server.py
+++ server.py
@@ -7,48 +7,74 @@ from threading import Thread
 from flask import Flask, render_template, session, request
 from flask.ext.socketio import *
 
+from random import randint
+
 app = Flask(__name__)
 app.template_folder = "."
 app.debug = True
 app.config["SECRET_KEY"] = "secret!"
 socketio = SocketIO(app)
-proc = None
-
-def createTimePackage(cur_time, dt, cnt):
-    # time is passed as a [hours, minutes, seconds] list
-    data = {}
-    data["hours"] = str(cur_time[0])
-    data["minutes"] = str(cur_time[1])
-    data["seconds"] = str(cur_time[2])
-    data["data"] = str(dt)
-    data["count"] = str(cnt)
-
-    json_data = json.dumps(data)
-
-    return json_data
+time_thread = None
+satellite_thread = None
 
 def broadcastTime():
     count = 0
+    json_data = {}
     while 1:
         time_string = time.strftime("%H:%M:%S")
         cur_time = [time_string[0:2], time_string[3:5], time_string[6:8]]
-        #json_data = createTimePackage(cur_time, "time", count)
-        time.sleep(0.1)
 
-        #print("Sending json package " + json_data)
-        socketio.emit("time broadcast",
-            {"data": "Server generated event", "count": count, "hours": cur_time[0],
-            "minutes": cur_time[1], "seconds": cur_time[2]},
-            namespace="/test")
+        # socketio.emit("time broadcast",
+        #     {"data": "Server generated event", "count": count, "hours": cur_time[0],
+        #     "minutes": cur_time[1], "seconds": cur_time[2]},
+        #     namespace="/test")
+
+        json_data = {
+            "data" : "Server time",
+            "count" : count,
+            "hours" : cur_time[0],
+            "minutes" : cur_time[1],
+            "seconds" : cur_time[2]
+        }
+
+        socketio.emit("time broadcast", json_data, namespace = "/test")
 
         count += 1
+        time.sleep(0.1)
+
+def broadcastSatellites():
+    count = 0
+    sat_number = 10
+    json_data = {}
+
+    while 1:
+
+        json_data = {
+            "data" : "Satellite levels"
+        }
+
+        for i in range(0, sat_number):
+            json_data["rover" + str(i)] = randint(35, 45)
+
+        for i in range(0, sat_number):
+            json_data["base" + str(i)] = randint(30, 40)
+
+        socketio.emit("satellite broadcast", json_data, namespace = "/test")
+        time.sleep(1)
 
 @app.route("/")
 def index():
-    global proc
-    if proc is None:
-        proc = Thread(target = broadcastTime)
-        proc.start()
+    global time_thread
+    global satellite_thread
+
+    if time_thread is None:
+        time_thread = Thread(target = broadcastTime)
+        time_thread.start()
+
+    if satellite_thread is None:
+        satellite_thread = Thread(target = broadcastSatellites)
+        satellite_thread.start()
+
     return render_template("index.html")
 
 @socketio.on("connect", namespace="/test")
--- static/main.js
+++ static/main.js
@@ -16,10 +16,24 @@ $(document).ready(function () {
     $("#lat_value").text("30");
     //$("#height_value").html("20");
 
-    var canvas1 = $("#sat_chart_canvas");
+    // This canvas contains the satellite_graph
 
-    var sat_data1 = {
-        labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13"],
+    var canvas = $("#sat_chart_canvas");
+    canvas.css("width", "99%");
+    canvas.css("margin", "1%");
+
+    var ctx = canvas.get(0).getContext("2d");
+
+    // keep aspect ratio
+    ctx.canvas.width = window.innerWidth;
+    ctx.canvas.height = 0.5 * window.innerWidth;
+
+    // change between-bar width depending on screen width
+    var bar_spacing = (ctx.canvas.width > 1000) ? 5 : 2;
+
+    // satellite_graph is created based on this data
+    var sat_data = {
+        labels: ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09"],
         datasets: [
             {
                 label: "Rover satellite levels",
@@ -27,7 +41,7 @@ $(document).ready(function () {
                 strokeColor: "rgba(220, 220, 220, 0.8)",
                 highlightFill: "rgba(220, 220, 220, 0.75)",
                 highlightStroke: "rgba(220, 220, 220, 1)",
-                data: [65, 59, 80, 81, 56, 55, 40, 31, 12, 15, 10, 80, 60, 54, 23, 12, 25, 36]
+                data: [50, 50, 50, 50, 50, 50, 50, 50, 50, 50]
             },
             {
                 label: "Base satellite levels",
@@ -35,12 +49,18 @@ $(document).ready(function () {
                 strokeColor: "rgba(151, 187, 205, 0.8)",
                 highlightFill: "rgba(151, 187, 205, 0.75)",
                 highlightStroke: "rgba(151, 187, 205, 1)",
-                data: [28, 48, 40, 19, 86, 27, 90, 31, 65, 59, 80, 81, 56, 55, 40, 31, 15, 10]
+                data: [50, 50, 50, 50, 50, 50, 50, 50, 50, 50]
             }
         ]
     };
 
-    drawSatChart(canvas1, sat_data1);
+    // draw the satellite_graph
+
+    var satellite_graph = new Chart(ctx).Bar(sat_data, {
+        responsive: true,
+        barDatasetSpacing: -1,
+        barValueSpacing: bar_spacing
+    });
 
     // handle data broadcast
 
@@ -50,6 +70,35 @@ $(document).ready(function () {
 
     socket.on("time broadcast", function (msg) {
         $("#height_value").html("<span>Received time: #" + msg.count + " " + msg.hours + ":" + msg.minutes + ":" + msg.seconds + "</span");
+    });
+
+    socket.on("satellite broadcast", function (msg) {
+        $("#mode_value").html("<span>Received rover1 val is " + msg.rover0 + "</span>")
+
+        // get all the keys of msg object
+        var msg_keys = [];
+        var rover_count = 0;
+        var base_count = 0;
+
+        for (var k in msg) {
+            msg_keys.push(k);
+            console.log(k);
+
+            if (k.indexOf("rover") > -1) {
+                console.log("Got rover message in " + k + " Data is " + msg[k] + " rover count is " + rover_count);
+                satellite_graph.datasets[0].bars[rover_count].value = msg[k];
+                rover_count++;
+            }
+
+            if (k.indexOf("base") > -1) {
+                console.log("Got rover message in " + k + " Data is " + msg[k] + " rover count is " + rover_count);
+                satellite_graph.datasets[1].bars[base_count].value = msg[k];
+                base_count++;
+            }
+
+        }
+
+        satellite_graph.update();
     });
 
 
