720f496ce6a99979a3b0a8c0345e9b5738521d61
Egor Fedorov <fedorovegor33@gmail.com>
2015-05-19 20:37:44 +0300

    additional features for rtk manager

--- RtkManager.py
+++ RtkManager.py
@@ -2,62 +2,119 @@ import pexpect
 
 class RtkManager:
 
-    def __init__(self, path_to_rtkrcv):
+    def __init__(self, path_to_rtkrcv = "/home/root/RTKLIB/app/rtkrcv/gcc"):
         self.bin_path = path_to_rtkrcv
         self.child = 0
-    
-    def start(self, config_name = "rtk.conf"):
-        print("./rtkrcv -o ../" + config_name)
-        self.child = pexpect.spawn("./rtkrcv -o ../" + config_name, cwd = self.bin_path)
-        r = self.child.expect(["rtkrcv> ", pexpect.EOF])
-        if r == 0:
-            print("good")
-        else:
+        self.status = {}
+        self.obs = {}
+
+    def expectAnswer(self, last_command = ""):
+        a = self.child.expect(["rtkrcv>", pexpect.EOF, "error"])
+        # check rtklib output for any errors
+        if a == 1:
             print("got EOF while waiting for rtkrcv> . Shutting down")
+            print("This means something went wrong and rtkrcv just stopped")
             print("output before exception: " + str(self.child))
+            return -1
+
+        if a == 2:
+            print("Could not " + last_command + ". Please check path to binary or config name")
+            print("You may also check serial port for availability")
+            return -2
+
+        return 1
+
+    def start(self, config_name = "rtk.conf"):
+
+        # run an rtkrcv instance with the specified config:
+        # if there is a slash in the name we consider it a full location
+        # otherwise, it's supposed to be in the upper directory(rtkrcv inside app)
+        if "/" in config_name:
+            self.child = pexpect.spawn("./rtkrcv -o " + config_name, cwd = self.bin_path, echo = False)
+        else:
+            self.child = pexpect.spawn("./rtkrcv -o ../" + config_name, cwd = self.bin_path, echo = False)
+
+        if self.expectAnswer("start") < 0:
             return -1
 
         print("launched rtklib")
         self.child.send("start\r\n")
-        r = self.child.expect(["rtkrcv> ", "error", pexpect.EOF])
-        if r == 0:
-            print("good")
-        else:
-            print("r=", r)
-            print("got EOF while waiting for rtkrcv> . Shutting down")
-            print("output before exception: " + str(self.child))
+
+        if self.expectAnswer("start") < 0:
             return -1
 
-        print("after start")
-
-        if "rtk server start error" in self.child.before:
-            print("too bad: something wrong about the config you specified")
-            return -1
-
-        self.child.sendline("status\r\n")
-        self.child.expect("rtkrcv>")
-
-        print("current status: " + self.child.before)
+        # started without rtklib catching  errors
+        print("RTKLIB launch and started succesful")
 
         return 1
 
     def restart(self):
-        self.child.sendline("restart\r\n")
-        self.child.expect("rtkrcv>")
+        self.child.send("restart\r\n")
 
-        if self.child.before == "rtk server start error":
-            print("too bad: something wrong about the config you specified")
+        if self.expectAnswer("restart") < 0:
             return -1
 
-        print("output = " + self.child.before)
+        print("RTKLIB restart successful")
+        print(self.child.before)
+
         return 1
 
-#    def loadConfig(self, config_name):
+    def loadConfig(self, config_name = "rtk.conf"):
+        self.child.send("load " + config_name + "\r\n")
 
-#    def stop(self):
+        if self.expectAnswer("load config"):
+            return -1
 
+        if self.restart() < 0:
+            return -1
+
+        return 1
+
+    def getStatus(self):
+        print("### Getting status ###")
+        self.child.send("status\r\n")
+
+        if self.expectAnswer("get status") < 0:
+            return -1
+
+        if self.expectAnswer("get status") < 0:
+            return -1
+
+        # time to extract information from the status form
+
+        print("Status == " + self.child.before)
+
+        status = self.child.before.split("\r\n")
+
+        for line in status:
+            spl = line.split(":")
+            print(spl)
+
+        print("@@@ Finished with status @@@")
+
+        return 1
+
+    def getObs(self):
+        print("### Getting Obs ###")
+        self.child.send("obs\r\n")
+
+        if self.expectAnswer("get obs") < 0:
+            return -1
+
+        # time to extract information from the obs form
+
+        print("Obs: " + self.child.before)
+
+        obs = self.child.before.split("\r\n")
+
+        for line in obs:
+            spl = line.split(":")
+            print(spl)
+
+        return 1
 
 rm = RtkManager("/Users/fedorovegor/Documents/RTKLIB/app/rtkrcv/gcc")
+
 if rm.start() > 0:
     rm.restart()
-
+    rm.getStatus()
