6d43ed16a9c47a13434ad1437c51d3b974e04642
Egor Fedorov <egor.fedorov@emlid.com>
2015-10-19 20:53:37 +0300

    made necessary changes to ConfigManager

--- ConfigManager.py
+++ ConfigManager.py
@@ -71,6 +71,7 @@ class ConfigItem:
         # form a line to put into a RTKLIB config file:
 
         # we want to write values aligned for easier reading
+        # hence need to add a number of spaces after the parameter
         parameter_with_trailing_spaces = self.parameter + " " * (18 - len(self.parameter))
 
         item = [parameter_with_trailing_spaces, "=" + self.value, "#", self.comment, "##", self.description]
@@ -83,17 +84,19 @@ class Config:
         # we keep all the options for current config file and their order here
 
         # config file name
-        self.file_name = ""
+        self.current_file_name = file_name
 
-        # items in the correct order
         self.items_list = []
 
     def readFromFile(self, from_file):
 
+        # save file name as current
+        self.current_file_name = from_file
+
         # clear previous data
         self.items_list = []
 
-        # item placeholder
+        # current item container
         item = ConfigItem()
 
         with open(from_file, "r") as f:
@@ -102,14 +105,18 @@ class Config:
                 item.extractFromString(line)
                 self.items_list.append(item)
 
-    def writeToFile(self, to_file):
+    def writeToFile(self, to_file = None):
+
+        # by default, we write to the current file
+        if to_file == None:
+            to_file = self.current_file_name
 
         with open(to_file, "w") as f:
             line = "# rtkrcv options for rtk (v.2.4.2)"
             f.write(line + "\n\n")
 
             for item in self.items_list:
-                f.writeline(item.formString() + "\n")
+                f.write(item.formString() + "\n")
 
 class ConfigManager:
 
@@ -125,11 +132,10 @@ class ConfigManager:
         self.available_configs = []
         self.updateAvailableConfigs()
 
-        self.buff_dict_comments = {}
-        self.buff_dict_human_comments = {}
-        self.buff_dict = {}
-        self.buff_dict_order = []
-        self.readConfig(self.default_rover_config) # we do this to load config order from default reach base config
+        # create a buffer for keeping config data
+        # read default one into buffer
+        self.buffered_config = Config()
+        self.buffered_config.readFromFile(self.default_rover_config)
 
     def updateAvailableConfigs(self):
 
@@ -155,48 +161,12 @@ class ConfigManager:
         else:
             config_file_path = self.config_path + from_file
 
-        self.buff_dict_order = []
+        self.buffered_config.readFromFile(from_file)
 
-        self.buff_dict = {}
-        self.buff_dict_comments = {}
-        self.buff_dict_human_comments = {}
+    def writeConfig(self, to_file, config_value = None):
 
-        with open(config_file_path, "r") as f:
-            for line in f:
-                separated_lines = line.split() # separate lines with spaces, get rid of extra whitespace
-                length = len(separated_lines)
-
-                # check if the line is empty or commented
-                if length > 0:
-                    if separated_lines[0][0] != "#":
-                        param = separated_lines[0] # get first part of the line, before the equal sign
-                        val = separated_lines[1][1:] # get the second part of the line, discarding "=" symbol
-
-                        if length > 3:
-                            comments = separated_lines[3]
-                            # some of the options are comments only
-                            # frontend need to check if the comment has ":" in it
-                            # then, it can be used to generate a dropdown menu
-                            # if ":" in options:
-
-                            self.buff_dict_comments[param] = comments
-
-                            if length > 5:
-                                # some lines will have a ## comment after the first one
-                                # to add more information and be more informative for ReachView
-                                human_comment = separated_lines[5]
-
-                                self.buff_dict_human_comments[param] = human_comment
-
-                        self.buff_dict[param] = val
-                        self.buff_dict_order.append(param) # this is needed to conserve the order of the parameters in the config file
-
-            self.buff_dict["config_file_name"] = from_file
-
-    def writeConfig(self, to_file, dict_values = None):
-
-        if dict_values is None:
-            dict_values = self.buff_dict
+        if config_value is None:
+            config_value = self.buffered_config
 
         if to_file is None:
             to_file = self.default_rover_config
@@ -208,24 +178,19 @@ class ConfigManager:
         else:
             config_file_path = self.config_path + to_file
 
-        print("Printing config we are about to write to " + config_file_path + "\n" + str(dict_values))
+        # do the actual writing
+        if config_value is None:
+            self.buffered_config.writeToFile(to_file)
+        else:
+            self.config_value.writeToFile(to_file)
 
-        with open(config_file_path, "w") as f:
-            line = "# rtkrcv options for rtk (v.2.4.2)"
-            f.write(line + "\n\n")
 
-            for key in self.buff_dict_order:
 
-                k = str(key)
-                if k in dict_values:
-                    v = str(dict_values[key])
 
-                    line = k + " " * (19 - len(k)) + "=" + v
 
-                    print("line = " + line)
-                    # check if comments are available
-                    if key in self.buff_dict_comments:
-                        line += " # " + self.buff_dict_comments[key]
 
-                    f.write(line + "\n")
+
+
+
+
 
