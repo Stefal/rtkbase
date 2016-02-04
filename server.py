c4b42a765cbab19a622743275ee0a1a263aba809
Egor Fedorov <egor.fedorov@emlid.com>
2016-02-04 17:00:11 +0300

    Add image version to settings tab

--- server.py
+++ server.py
@@ -31,6 +31,7 @@ import json
 import os
 import signal
 import sys
+import ReachTools
 
 from RTKLIB import RTKLIB
 from port import changeBaudrateTo230400
@@ -39,7 +40,6 @@ from threading import Thread
 from flask import Flask, render_template, session, request, send_file
 from flask.ext.socketio import SocketIO, emit, disconnect
 from subprocess import check_output
-from NetworkTools import getNetworkStatus
 
 app = Flask(__name__)
 app.template_folder = "."
@@ -54,10 +54,6 @@ changeBaudrateTo230400()
 
 rtk = RTKLIB(socketio)
 
-# Extract git tag as software version
-git_tag_cmd = "git describe --tags"
-app_version = check_output([git_tag_cmd], shell = True, cwd = "/home/reach/ReachView")
-
 # at this point we are ready to start rtk in 2 possible ways: rover and base
 # we choose what to do by getting messages from the browser
 
@@ -66,7 +62,7 @@ def index():
     print("INDEX DEBUG")
     rtk.logm.updateAvailableLogs()
     print("AVAILABLE LOGS == " + str(rtk.logm.available_logs))
-    return render_template("index.html", logs = rtk.logm.available_logs, app_version = app_version, network_status = getNetworkStatus())
+    return render_template("index.html", logs = rtk.logm.available_logs, system_status = ReachTools.getSystemStatus())
 
 # @app.route("/logs/<path:log_name>")
 # def processLog(log_name):
