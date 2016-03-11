9f5ab88c7eb7969d062e2f436bb5ee7e13bd2bd8
Egor Fedorov <egor.fedorov@emlid.com>
2016-03-11 19:49:24 +0300

    live pairing

--- bluetooth/bluetooth.py
+++ bluetooth/bluetooth.py
@@ -16,11 +16,12 @@ class Bluetooth:
     def initialize(self):
         """Starts a bluetoothctl process. Raise BluetoothError on failure."""
         self.child = pexpect.spawn("bluetoothctl", echo = False)
-        # self.child.logfile = sys.stdout
+        self.child.logfile = sys.stdout
 
-    def get_output(self, command):
+    def get_output(self, command, pause = 0):
         """Run a command in bluetoothctl prompt, return output as a list of lines."""
         self.child.send(command + "\n")
+        time.sleep(pause)
         start_failed = self.child.expect(["bluetooth", pexpect.EOF])
 
         if start_failed:
@@ -52,12 +53,33 @@ class Bluetooth:
 
             return available_devices
 
+    # def pair(self, mac_address):
+    #     self.child.send("pair " + mac_address + "\r\n")
+    #     time.sleep(3)
+    #     start_failed = self.child.expect(["#", pexpect.EOF])
+
+    #     if start_failed:
+    #         raise BluetoothError("Bluetoothctl failed after running pair " + mac_address)
+
+    #     return self.child.before.split("\r\n")
+
     def pair(self, mac_address):
         try:
-            out = self.get_output("pair " + mac_address)
+            out = self.get_output("pair " + mac_address + "\r\n", 3)
         except BluetoothError, e:
             print(e)
             return None
+        else:
+            return out
+
+    def get_device_info(self, mac_address):
+        try:
+            out = self.get_output("info " + mac_address)
+        except BluetoothError, e:
+            print(e)
+            return None
+        else:
+            return out
 
 
 def pretty_print(list_to_print):
@@ -81,6 +103,33 @@ if __name__ == "__main__":
         print("WTF")
         pretty_print(bl.get_output("help"))
         pretty_print(bl.get_output("scan on"))
-        time.sleep(5)
-        print(bl.get_available_devices())
+        time.sleep(8)
+        devices = bl.get_available_devices()
+        pretty_print(devices)
+        target_mac = ""
+        for dev in devices:
+            if dev[1] == "Galaxy":
+                target_mac = dev[0]
+
+        print("pairing with " + target_mac)
+        pretty_print(bl.get_device_info(target_mac))
+        print("Finally")
+        pretty_print(bl.pair(target_mac))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
