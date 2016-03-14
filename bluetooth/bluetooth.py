b87d43f965a09ce4ec34633018e6acc107f21d68
Egor Fedorov <egor.fedorov@emlid.com>
2016-03-14 16:15:35 +0300

    add connect, fix bugs, revamp main

--- bluetooth/bluetooth.py
+++ bluetooth/bluetooth.py
@@ -16,7 +16,6 @@ class Bluetooth:
     def initialize(self):
         """Starts a bluetoothctl process. Raise BluetoothError on failure."""
         self.child = pexpect.spawn("bluetoothctl", echo = False)
-        self.child.logfile = sys.stdout
 
     def get_output(self, command, pause = 0):
         """Run a command in bluetoothctl prompt, return output as a list of lines."""
@@ -46,22 +45,12 @@ class Bluetooth:
             available_devices = []
             for line in out:
                 if line.startswith("Device"):
-                    attribute_list = line.split()
+                    attribute_list = line.split(" ", 2)
                     mac_address = attribute_list[1]
                     name = attribute_list[2]
                     available_devices.append((mac_address, name))
 
             return available_devices
-
-    # def pair(self, mac_address):
-    #     self.child.send("pair " + mac_address + "\r\n")
-    #     time.sleep(3)
-    #     start_failed = self.child.expect(["#", pexpect.EOF])
-
-    #     if start_failed:
-    #         raise BluetoothError("Bluetoothctl failed after running pair " + mac_address)
-
-    #     return self.child.before.split("\r\n")
 
     def pair(self, mac_address):
         try:
@@ -81,40 +70,45 @@ class Bluetooth:
         else:
             return out
 
+    def connect(self, mac_address):
+        try:
+            out = self.get_output("connect " + mac_address + "\r\n", 2)
+        except BluetoothError, e:
+            print(e)
+            return None
+        else:
+            return out
 
-def pretty_print(list_to_print):
-    print("\nstart")
-    for line in list_to_print:
-        print([line])
-    print("stop")
 
 if __name__ == "__main__":
 
     print("Init bluetooth...")
     bl = Bluetooth()
     print("Ready!")
-    try:
-        bl.initialize()
-    except BluetoothError, e:
-        print(e)
-    else:
-        print("Successful init!")
-        pretty_print(bl.get_output("devices"))
-        print("WTF")
-        pretty_print(bl.get_output("help"))
-        pretty_print(bl.get_output("scan on"))
-        time.sleep(8)
-        devices = bl.get_available_devices()
-        pretty_print(devices)
-        target_mac = ""
-        for dev in devices:
-            if dev[1] == "Galaxy":
-                target_mac = dev[0]
+    bl.initialize()
+    print("Successful init!")
+    pretty_print(bl.get_output("scan on"))
+    print("Scan turned on")
 
-        print("pairing with " + target_mac)
-        pretty_print(bl.get_device_info(target_mac))
-        print("Finally")
-        pretty_print(bl.pair(target_mac))
+    searching_for_air = True
+    while searching_for_air:
+        print("Scanning...")
+        devices = bl.get_available_devices()
+        print(devices)
+        for mac, name in devices:
+            if name == "Galaxy Note3":
+                searching_for_air = False
+                break
+
+    print("Found air!")
+
+    pretty_print(bl.get_device_info(mac))
+    print("Finally pairing...")
+    pretty_print(bl.pair(mac))
+
+    time.sleep(3)
+    print("Connecting...")
+    pretty_print(bl.connect(mac))
 
 
 
