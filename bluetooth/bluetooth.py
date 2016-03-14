61c4e803ae27951835c657d1ff4548183003f3ae
Egor Fedorov <egor.fedorov@emlid.com>
2016-03-14 17:20:12 +0300

    add remove, disconnect, list of paired devices, docstrings

--- bluetooth/bluetooth.py
+++ bluetooth/bluetooth.py
@@ -11,10 +11,6 @@ class Bluetooth:
 
     def __init__(self):
         out = subprocess.check_output("rfkill unblock bluetooth", shell = True)
-        self.child = None
-
-    def initialize(self):
-        """Starts a bluetoothctl process. Raise BluetoothError on failure."""
         self.child = pexpect.spawn("bluetoothctl", echo = False)
 
     def get_output(self, command, pause = 0):
@@ -29,13 +25,23 @@ class Bluetooth:
         return self.child.before.split("\r\n")
 
     def start_scan(self):
+        """Start bluetooth scanning process."""
         try:
             out = self.get_output("scan on")
         except BluetoothError, e:
             print(e)
             return None
 
+    def make_discoverable(self):
+        """Make device discoverable."""
+        try:
+            out = self.get_output("discoverable on")
+        except BluetoothError, e:
+            print(e)
+            return None
+
     def get_available_devices(self):
+        """Return a list of tuples of nearby discoverable devices."""
         try:
             out = self.get_output("devices")
         except BluetoothError, e:
@@ -52,7 +58,26 @@ class Bluetooth:
 
             return available_devices
 
+    def get_paired_devices(self):
+        """Return a list of tuples of paired devices."""
+        try:
+            out = self.get_output("paired-devices")
+        except BluetoothError, e:
+            print(e)
+            return None
+        else:
+            available_devices = []
+            for line in out:
+                if line.startswith("Device"):
+                    attribute_list = line.split(" ", 2)
+                    mac_address = attribute_list[1]
+                    name = attribute_list[2]
+                    available_devices.append((mac_address, name))
+
+            return available_devices
+
     def pair(self, mac_address):
+        """Try to pair with a device by mac address."""
         try:
             out = self.get_output("pair " + mac_address + "\r\n", 3)
         except BluetoothError, e:
@@ -61,7 +86,18 @@ class Bluetooth:
         else:
             return out
 
+    def remove(self, mac_address):
+        """Remove paired device by mac address, return success of the operation."""
+        try:
+            out = self.get_output("remove " + mac_address + "\r\n", 3)
+        except BluetoothError, e:
+            print(e)
+            return None
+        else:
+            return out
+
     def get_device_info(self, mac_address):
+        """Get device info by mac address."""
         try:
             out = self.get_output("info " + mac_address)
         except BluetoothError, e:
@@ -71,8 +107,19 @@ class Bluetooth:
             return out
 
     def connect(self, mac_address):
+        """Try to connect to a device by mac address."""
         try:
             out = self.get_output("connect " + mac_address + "\r\n", 2)
+        except BluetoothError, e:
+            print(e)
+            return None
+        else:
+            return out
+
+    def disconnect(self, mac_address):
+        """Try to disconnect to a device by mac address."""
+        try:
+            out = self.get_output("disconnect " + mac_address + "\r\n", 2)
         except BluetoothError, e:
             print(e)
             return None
@@ -85,8 +132,6 @@ if __name__ == "__main__":
     print("Init bluetooth...")
     bl = Bluetooth()
     print("Ready!")
-    bl.initialize()
-    print("Successful init!")
     pretty_print(bl.get_output("scan on"))
     print("Scan turned on")
 
