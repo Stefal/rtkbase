981c5aab7abda0a6ef93a59ec564426e80062cce
Egor Fedorov <egor.fedorov@emlid.com>
2016-03-18 15:38:18 +0000

    Working bluetooth serial server

--- bluetooth_serial/bluetooth_serial.py
+++ bluetooth_serial/bluetooth_serial.py
@@ -1,32 +1,19 @@
-import time
 import bluetooth
 import subprocess
 import multiprocessing
-import threading
+import socket
+
+class BluetoothConnectionError(Exception):
+    pass
 
 class BluetoothSerial:
 
-    def __init__(self, socketio):
+    def __init__(self, buffer_size=1024):
         out = subprocess.check_output("rfkill unblock bluetooth", shell = True)
+
         self.server_socket = None
-        self.socketio = socketio
-        self.scan_process = None
-        self.watch_thread = None
-        self.scan_queue = multiprocessing.Queue()
-
-    def scan(self):
-        devices = bluetooth.discover_devices(lookup_names = True, duration = 5)
-        devices_dict = {}
-        i = 0
-        for address, name in devices:
-            dev_dict = {
-                "mac_address": address,
-                "name": name
-            }
-            devices_dict.update({i: dev_dict})
-            i += 1
-
-        self.socketio.emit("bluetooth scan results", devices_dict, namespace="/test")
+        self.client_socket = None
+        self.buffer_size = buffer_size
 
     def initialize_server(self):
         self.server_socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
@@ -34,7 +21,6 @@ class BluetoothSerial:
         self.server_socket.listen(1)
 
         port = self.server_socket.getsockname()[1]
-
         uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
 
         bluetooth.advertise_service(
@@ -45,29 +31,86 @@ class BluetoothSerial:
             profiles = [bluetooth.SERIAL_PORT_PROFILE],
         )
 
+        print("Initialized bluetooth serial server")
         print("Waiting for connection on RFCOMM channel " + str(port))
 
     def accept_connection(self):
-        client_socket, client_info = self.server_socket.accept()
+        self.client_socket, client_info = self.server_socket.accept()
         print("Accepted connection from " + str(client_info))
 
-        try:
-            while True:
-                data = client_socket.recv(1024)
-                if len(data) == 0: break
-                print("received [%s]" % data)
-        except IOError:
-            pass
-
-        print("disconnected")
-
-        client_socket.close()
+    def kill_connection(self):
+        self.client_socket.close()
         self.server_socket.close()
-        print("all done")
+        self.client_socket = None
+        self.server_socket = None
+
+    def read(self):
+        return self.client_socket.recv(self.buffer_size)
+
+    def write(self, data):
+        return self.client_socket.send(data)
+
+class BluetoothTCPBridge:
+
+    def __init__(self, tcp_port=8143):
+        self.tcp_port = tcp_port
+        self.socket = None
+        self.bluetooth_serial = BluetoothSerial()
+
+        self.bridge_not_interrupted = True
+        self.bridge_process = None
+
+    def connect_tcp(self):
+        self.socket = socket.create_connection(("localhost", self.tcp_port))
+
+    def kill_connections(self):
+        self.socket.shutdown(socket.SHUT_RDWR)
+        self.socket.close()
+        self.bluetooth_serial.kill_connection()
+
+    def bridge(self):
+        while bridge_not_interrupted:
+            print("Initializing bluetooth server...")
+            self.bluetooth_serial.initialize_server()
+            self.bluetooth_serial.accept_connection()
+            self.connect_tcp()
+
+            try:
+                while True:
+                    data_received = self.socket.recv(1024)
+                    print("Received data via tcp: " + str(data_received))
+                    self.bluetooth_serial.write(data_received)
+            except IOError, e:
+                print(e)
+                self.kill_connections()
+                print("Bluetooth connection broken, reinitializing...")
+
+    def start(self):
+        self.bridge_not_interrupted = False
+        self.bridge_process = multiprocessing.Process(target = self.bridge)
+        self.bridge_process.start()
+
+    def stop(self):
+        self.bridge_not_interrupted = False
+        self.bridge_process.join()
+        self.bridge_process = None
+
 
 if __name__ == "__main__":
-    bl = BluetoothSerial(0)
-    bl.start_scan_thread()
-    for i in range(0, 20):
-        print(i)
-        time.sleep(1)
+    bl = BluetoothTCPBridge()
+    try:
+        bl.run()
+    except KeyboardInterrupt:
+        bl.kill()
+        print("Deinited")
+
+    print("Program exiting...")
+
+
+
+
+
+
+
+
+
