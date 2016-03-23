4b6bd763c9664edaf2969d3814b5de66b90e50aa
Egor Fedorov <egor.fedorov@emlid.com>
2016-03-23 19:13:21 +0300

    update install packages to change bluetoothd service file

--- install_packages.sh
+++ install_packages.sh
@@ -4,3 +4,6 @@ echo "Installing new packages"
 opkg update
 opkg install kernel-module-ftdi-sio
 pip install pybluez
+
+echo "Reconfigure bluetoothd"
+sed -i s/'ExecStart=\/usr\/lib\/bluez5\/bluetooth\/bluetoothd'/'ExecStart=\/usr\/lib\/bluez5\/bluetooth\/bluetoothd -C'/g /etc/systemd/system/bluetooth.target.wants/bluetooth.service
