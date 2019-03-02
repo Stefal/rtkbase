6401ca5c5f0d9bcfa4298b5d651ac7c1af2e7261
Stefal <stephane.peneau@wanadoo.fr>
2019-03-02 19:55:08 +0100

    initial

---
+++ loop.sh
@@ -0,0 +1,5 @@
+while true
+do
+echo "loop"
+sleep 1
+done
---
+++ network_check.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+uart='/dev/ttyS1'
+iface='enp0s3'
+svr_list='osm.org osm.fr osm.de'
+iface_up=0
+wan=0
+
+ping_svr()
+{
+  ping -q -w2 ${1} >/dev/null 2>&1
+  if [ ${?} -eq 0 ]
+    then local answer=1
+    else local answer=0
+  fi
+  echo ${answer}
+}
+
+ping_svr_list()
+{
+
+  for addr in ${svr_list}
+  do
+    if [ $(ping_svr $addr) -eq 1 ]
+      then local answer=1
+           break
+      else local answer=0
+    fi
+  done
+  echo ${answer}
+}
+
+
+is_lan_up()
+{
+  if $(ip address | grep -Eq ": $iface:.*state UP")
+    then local answer=1
+    else local anwer=0
+  fi
+  echo ${answer}
+}
+
+is_time_sync()
+{
+  if $(timedatectl status | grep -q "synchronized: yes")
+    then local answer=1
+    else local answer=0
+  fi
+  echo ${answer}
+}
+
+
+
+
+waiting_loop()
+#parameter 1: function to test
+#parameter 2: timeout value
+
+{
+  i=0
+  while [ $i -le ${2} ]
+  do
+    if [[ $(${1}) == 1 ]]
+      then
+        echo 1
+        break
+    fi
+    ((i++))
+    sleep 1
+  done
+}
+
+wl2()
+#parameter 1: function to test
+#parameter 2: timeout value
+
+{
+  i=0
+  while [ $i -le ${2} ]
+  do
+    echo $1
+    ((i++))
+    sleep 1
+  done
+}
+
+main()
+{
+echo 'lan: ' $(waiting_loop is_lan_up 60)
+echo 'time sync: ' $(waiting_loop is_time_sync 60)
+echo 'wan up: ' $(waiting_loop ping_svr_list 60)
+#echo 'wan up: ' $(wl2 ping_svr_list 60)
+}
+
+main
+
