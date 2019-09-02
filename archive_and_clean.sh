f44bdeb08669b36e3aea2cdfded65f2c5ff25e07
Stefal <stephane.peneau@wanadoo.fr>
2019-09-02 21:52:45 +0200

    Update archive_and_clean.sh

--- archive_and_clean.sh
+++ archive_and_clean.sh
@@ -9,4 +9,4 @@ archive_name=$(date -d "-1 days" +"%Y-%m-%d_%S").tar.bz2
 find . -maxdepth 1 -type f -mtime -1 -mmin +60 -name "*.ubx*" -exec tar -jcvf ${archive_name} --remove-files {} +;
 
 #delete gnss data older than 30 days
-find . -maxdepth 1 -type f -name "*.tar.bz2" -mtime +10 -delete
+find . -maxdepth 1 -type f -name "*.tar.bz2" -mtime +30 -delete
