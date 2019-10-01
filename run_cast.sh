0aa9f38b1f0ac28428a89a669a1297441cd502cb
Stefal <stephane.peneau@wanadoo.fr>
2019-10-01 21:08:47 +0200

    moving settings to settings.conf

--- run_cast.sh
+++ run_cast.sh
@@ -5,35 +5,8 @@
 # https://github.com/tomojitakasu/RTKLIB
 
 
-# NTRIP caster program
-BASEDIR=$(dirname "$0")
-cast=/usr/local/bin/str2str
+source ./settings.conf
 
-# General options
-position='47.034 -1.251 36.4'			#base coordinates: lat long height
-com_port='ttyS1:115200:8:n:1'			#gnss receiver com port and settings
-receiver_format='ubx'				#gnss receiver format
-serial_input="${com_port}#${receiver_format}"
-
-# File options
-datadir=$BASEDIR/data				#gnss data directory
-file_name="%Y-%m-%d-%h:%M:%S-GNSS-1.ubx"	#gnss data filename
-file_rotate_time=24				#file rotate time in hour
-file_overlap_time=30				#file overlap time in seconds
-
-# TCP options
-tcp_port=5015
-
-# NTRIP caster options
-svr_addr=caster.centipede.fr	#ntrip caster url
-svr_port=2101					#ntrip caster port
-svr_pwd=none    				#ntrip caster password
-mnt_name=Your_mount_name			#Mount name
-receiver=Ublox_neo-m8t				#receiver model
-rtcm_msg='1004,1005,1006,1012,1019,1020,1042,1045,1046,1077,1087,1097,1107,1127'
-
-logdir=$BASEDIR/log				#log directory
-level=0						#trace level (0: no trace)
 
 in_serial="serial://${serial_input}"
 in_tcp="tcpcli://127.0.0.1:${tcp_port}#${receiver_format}"
@@ -42,10 +15,7 @@ out_caster="ntrips://:${svr_pwd}@${svr_addr}:${svr_port}/${mnt_name}#rtcm3 -msg 
 out_tcp="tcpsvr://:${tcp_port}"
 out_file="file://${datadir}/${file_name}::T::S=${file_rotate_time} -f ${file_overlap_time}"
 
-# NTRIP caster logs
-log1=$logdir/cast_trac_`date -u +%Y%m%d_%H%M%S`.log
-log2=$logdir/cast_stat_`date -u +%Y%m%d_%H%M%S`.log
-log3=$logdir/ifconfig_`date -u +%Y%m%d_%H%M%S`.log
+
 # start NTRIP caster
 
 
