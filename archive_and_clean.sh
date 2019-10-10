68b05c54ed8a12ae346f9fb5800991bd764d02af
Stefal <stephane.peneau@wanadoo.fr>
2019-10-10 10:31:51 +0000

    Moving variable from archive_and_clean to settings.conf

--- archive_and_clean.sh
+++ archive_and_clean.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #This script should be run from a crontab
+#You can customize archive_name and archive_duration in settings.conf
 
 BASEDIR=$(dirname "$0")
 source ${BASEDIR}/settings.conf
 cd ${datadir}
 
 #archive and compress previous day's gnss data.
-archive_name=$(date -d "-1 days" +"%Y-%m-%d_%S").tar.bz2
 find . -maxdepth 1 -type f -mtime -1 -mmin +60 -name "*.ubx*" -exec tar -jcvf ${archive_name} --remove-files {} +;
 
-#delete gnss data older than 30 days
-find . -maxdepth 1 -type f -name "*.tar.bz2" -mtime +30 -delete
+#delete gnss data older than x days.
+find . -maxdepth 1 -type f -name "*.tar.bz2" -mtime +${archive_rotate} -delete
+
