c34b222850249231e620c0a68175f5a73a9c5a42
Stefal <stephane.peneau@wanadoo.fr>
2020-04-29 22:56:55 +0200

    updating update procedure

--- rtkbase_update.sh
+++ rtkbase_update.sh
@@ -5,25 +5,25 @@
 destination_directory=$1
 data_dir=$2
 
-#remove existing rtkbase.old directory
+echo "remove existing rtkbase.old directory"
 rm -rf /var/tmp/rtkbase.old
 mkdir /var/tmp/rtkbase.old
 
-#copy rtkbase to rtkbase.old except /data directory
+echo "copy rtkbase to rtkbase.old except /data directory"
 #'shopt -s extglob' is needed for using (!pattern) exclusion pattern
 #from inside a script
 shopt -s extglob
 cp -r ${destination_directory}/!(${data_dir}) /var/tmp/rtkbase.old
 
-#copy new release to destination
+echo "copy new release to destination"
 cp -rfp * ${destination_directory}
 
-#delete the line version= in settings.conf
+echo "delete the line version= in settings.conf"
 sed -i '/version=/d' ${destination_directory}/settings.conf
 #if a reboot is needed
 #sudo reboot now
 
-#if a reboot isn't needed
+echo "Restart web server"
 sudo systemctl restart rtkbase_web.service
 
 
--- web_app/server.py
+++ web_app/server.py
@@ -186,7 +186,7 @@ def update_rtkbase():
     rtk.shutdownBase()
     rtkbase_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../"))
     script_path = os.path.join("/var/tmp/", primary_folder, "rtkbase_update.sh")
-    os.execl(script_path, rtkbase_path, app.config["DOWNLOAD_FOLDER"].split("/")[-1])
+    os.execl(script_path, "unused arg0", rtkbase_path, app.config["DOWNLOAD_FOLDER"].split("/")[-1])
 
 # at this point we are ready to start rtk in 2 possible ways: rover and base
 # we choose what to do by getting messages from the browser
