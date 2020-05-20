dddcecd050b6429cadc61f04d07d75cefa8d485a
jancelin <julien.ancelin@inra.fr>
2020-05-20 17:45:50 +0200

    install: change help, add tests, unmerge detect/flash, rm set -x

--- tools/install.sh
+++ tools/install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#set -x
 
 ### RTKBASE INSTALLATION SCRIPT ###
 declare -a detected_gnss
@@ -40,8 +39,16 @@ man_help(){
     echo '        --unit-files'
     echo '                         Deploy services.'
     echo ''
+    echo '        --crontab'
+    echo '                         add crontab tools, every day logs are store'
+    echo ''
+    echo ''
+    echo '        --detect-usb-gnss'
+    echo '                         Detect your GNSS receiver.'
+    echo ''
+    echo ''
     echo '        --flash-gnss'
-    echo '                         Detect your GNSS receiver and flash it.'
+    echo '                         Flash your GNSS receiver.'
     echo ''
     echo
 
@@ -80,21 +87,57 @@ install_rtklib() {
       fi
 }
 
+rtkbase_repo(){
+    #Get rtkbase repository
+    sudo -u $(logname) git clone -b web_gui --single-branch https://github.com/stefal/rtkbase.git
+}
+
+rtkbase_release(){
+    #Get rtkbase latest release
+    sudo -u $(logname) wget https://github.com/stefal/rtkbase/releases/latest/download/rtkbase.tar.gz -O rtkbase.tar.gz
+    sudo -u $(logname) tar -xvf rtkbase.tar.gz
+}
+
 install_rtkbase_from_repo() {
     echo '################################'
     echo 'INSTALLING RTKBASE FROM REPO'
     echo '################################'
-      #Get rtkbase repository
-      sudo -u $(logname) git clone -b web_gui --single-branch https://github.com/stefal/rtkbase.git
+      if [ -d rtkbase ]
+      then
+        if [ -d rtkbase/.git ]
+        then
+          echo "RtkBase repo: YES, git pull"
+          git -C rtkbase pull
+        else
+          echo "RtkBase repo: NO, rm release & git clone rtkbase"
+          rm -r rtkbase
+          rtkbase_repo
+        fi
+      else
+        echo "RtkBase repo: NO, git clone rtkbase"
+        rtkbase_repo
+      fi
 }
 
 install_rtkbase_from_release() {
     echo '################################'
     echo 'INSTALLING RTKBASE FROM REALEASE'
     echo '################################'
-      #Get rtkbase latest release
-      sudo -u $(logname) wget https://github.com/stefal/rtkbase/releases/latest/download/rtkbase.tar.gz -O rtkbase.tar.gz
-      sudo -u $(logname) tar -xvf rtkbase.tar.gz
+      if [ -d rtkbase ]
+      then
+        if [ -d rtkbase/.git ]
+        then
+          echo "RtkBase release: NO, rm repo & download last release"
+          rm -r rtkbase
+          rtkbase_release
+        else
+          echo "RtkBase release: YES, rm & deploy last release"
+          rtkbase_release
+        fi
+      else
+        echo "RtkBase release: NO, download & deploy last release"
+        rtkbase_release
+      fi
 }
 
 rtkbase_requirements(){
@@ -113,61 +156,82 @@ install_unit_files() {
     echo '################################'
     echo 'ADDING UNIT FILES'
     echo '################################'
-      #Install unit files
-      /home/$(logname)/rtkbase/copy_unit.sh
-      systemctl enable rtkbase_web.service
-      systemctl daemon-reload
-      systemctl start rtkbase_web.service
+      if [ -d rtkbase ]
+      then 
+        #Install unit files
+        /home/$(logname)/rtkbase/copy_unit.sh
+        systemctl enable rtkbase_web.service
+        systemctl daemon-reload
+        systemctl start rtkbase_web.service
+      else
+        echo 'RtkBase not installed, use option --rtkbase-release'
+      fi
 }
 
 add_crontab() {
     echo '################################'
     echo 'ADDING CRONTAB'
     echo '################################'
-      #script from https://stackoverflow.com/questions/610839/how-can-i-programmatically-create-a-new-cron-job
-      #I've added '-r' to sort because SHELL=/bin/bash should stay before "0 4 * * ..."
-      (crontab -u $(logname) -l ; echo 'SHELL=/bin/bash') | sort -r | uniq - | crontab -u $(logname) -
-      (crontab -u $(logname) -l ; echo "0 4 * * * $(eval echo ~$(logname)/rtkbase/archive_and_clean.sh)") | sort -r | uniq - | crontab -u $(logname) -
+      if [ -d rtkbase ]
+      then 
+        #script from https://stackoverflow.com/questions/610839/how-can-i-programmatically-create-a-new-cron-job
+        #I've added '-r' to sort because SHELL=/bin/bash should stay before "0 4 * * ..."
+        (crontab -u $(logname) -l ; echo 'SHELL=/bin/bash') | sort -r | uniq - | crontab -u $(logname) -
+        (crontab -u $(logname) -l ; echo "0 4 * * * $(eval echo ~$(logname)/rtkbase/archive_and_clean.sh)") | sort -r | uniq - | crontab -u $(logname) -
+      else
+        echo 'RtkBase not installed, use option --rtkbase-release'
+      fi
 }
 
 detect_usb_gnss() {
     echo '################################'
     echo 'GNSS RECEIVER DETECTION'
     echo '################################'
-      #This function put the (USB) detected gnss receiver informations in detected_gnss
-      #If there are several receiver, only the last one will be present in the variable
-      for sysdevpath in $(find /sys/bus/usb/devices/usb*/ -name dev); do
-        syspath="${sysdevpath%/dev}"
-        devname="$(udevadm info -q name -p $syspath)"
-        if [[ "$devname" == "bus/"* ]]; then continue; fi
-        eval "$(udevadm info -q property --export -p $syspath)"
-        if [[ -z "$ID_SERIAL" ]]; then continue; fi
-        if [[ "$ID_SERIAL" =~ ^(U-blox|skytraq)$ ]]; then continue; fi
-        detected_gnss[0]=$devname
-        detected_gnss[1]=$ID_SERIAL
-     done
+      if [ -d rtkbase ]
+      then 
+        #This function put the (USB) detected gnss receiver informations in detected_gnss
+        #If there are several receiver, only the last one will be present in the variable
+        for sysdevpath in $(find /sys/bus/usb/devices/usb*/ -name dev); do
+          syspath="${sysdevpath%/dev}"
+          devname="$(udevadm info -q name -p $syspath)"
+          if [[ "$devname" == "bus/"* ]]; then continue; fi
+          eval "$(udevadm info -q property --export -p $syspath)"
+          if [[ -z "$ID_SERIAL" ]]; then continue; fi
+          if [[ "$ID_SERIAL" =~ ^(U-blox|skytraq)$ ]]; then continue; fi
+          detected_gnss[0]=$devname
+          detected_gnss[1]=$ID_SERIAL
+          echo '/dev/'${detected_gnss[0]} ' - ' ${detected_gnss[1]}
+        done
+      else
+        echo 'RtkBase not installed, use option --rtkbase-release'
+      fi
 }
 
 flash_gnss(){
     echo '################################'
     echo 'FLASH GNSS RECEIVER'
     echo '################################'
-      if [[ ${#detected_gnss[*]} -eq 2 ]]
-      then
-        echo 'GNSS RECEIVER DETECTED: /dev/'${detected_gnss[0]} ' - ' ${detected_gnss[1]}
-        if [[ -f "rtkbase/settings.conf" ]]  #check if settings.conf exists
+      if [ -d rtkbase ]
+      then 
+        if [[ ${#detected_gnss[*]} -eq 2 ]]
         then
-          #inject the com port inside settings.conf
-          sudo -u $(logname) sed -i s/com_port=.*/com_port=\'${detected_gnss[0]}\'/ rtkbase/settings.conf
-        else
-          #create settings.conf with the com_port setting and the format
-          sudo -u $(logname) printf "[main]\ncom_port='"${detected_gnss[0]}"'\ncom_port_settings='115200:8:n:1'" > rtkbase/settings.conf
+          echo 'GNSS RECEIVER DETECTED: /dev/'${detected_gnss[0]} ' - ' ${detected_gnss[1]}
+          if [[ -f "rtkbase/settings.conf" ]]  #check if settings.conf exists
+          then
+            #inject the com port inside settings.conf
+            sudo -u $(logname) sed -i s/com_port=.*/com_port=\'${detected_gnss[0]}\'/ rtkbase/settings.conf
+          else
+            #create settings.conf with the com_port setting and the format
+            sudo -u $(logname) printf "[main]\ncom_port='"${detected_gnss[0]}"'\ncom_port_settings='115200:8:n:1'" > rtkbase/settings.conf
+          fi
         fi
-      fi
-      #if the receiver is a U-Blox, launch the set_zed-f9p.sh. This script will reset the F9P and flash it with the corrects settings for rtkbase
-      if [[ ${detected_gnss[1]} =~ 'u-blox' ]]
-      then
-        rtkbase/tools/set_zed-f9p.sh /dev/${detected_gnss[0]} 115200 rtkbase/receiver_cfg/U-Blox_ZED-F9P_rtkbase.txt
+        #if the receiver is a U-Blox, launch the set_zed-f9p.sh. This script will reset the F9P and flash it with the corrects settings for rtkbase
+        if [[ ${detected_gnss[1]} =~ 'u-blox' ]]
+        then
+          rtkbase/tools/set_zed-f9p.sh /dev/${detected_gnss[0]} 115200 rtkbase/receiver_cfg/U-Blox_ZED-F9P_rtkbase.txt
+        fi
+      else
+        echo 'RtkBase not installed, use option --rtkbase-release'
       fi
 }
 
@@ -181,22 +245,22 @@ main() {
   for i in "${array[@]}"
   do
     if [ "$1" == "--help" ]           ; then man_help                        ;fi
-    if [ "$i" == "--dependencies" ]   ; then install_dependencies	     ;fi
-    if [ "$i" == "--rtklib" ] 	      ; then install_rtklib		     ;fi
+    if [ "$i" == "--dependencies" ]   ; then install_dependencie             ;fi
+    if [ "$i" == "--rtklib" ] 	      ; then install_rtklib                  ;fi
     if [ "$i" == "--rtkbase-release" ]; then install_rtkbase_from_release && \
-					     rtkbase_requirements	     ;fi
+					     rtkbase_requirements            ;fi
     if [ "$i" == "--rtkbase-repo" ]   ; then install_rtkbase_from_repo    && \
-					     rtkbase_requirements	     ;fi
-    if [ "$i" == "--unit-files" ]     ; then install_unit_files	    	     ;fi
-    if [ "$i" == "--crontab" ] 	      ; then add_crontab		     ;fi
-    if [ "$i" == "--flash-gnss" ]     ; then detect_usb_gnss		  && \
-					     flash_gnss	    		     ;fi
+					     rtkbase_requirements            ;fi
+    if [ "$i" == "--unit-files" ]     ; then install_unit_files              ;fi
+    if [ "$i" == "--crontab" ] 	      ; then add_crontab                     ;fi
+    if [ "$i" == "--detect-usb-gnss" ]; then detect_usb_gnss                 ;fi
+    if [ "$i" == "--flash-gnss" ]     ; then flash_gnss                      ;fi
     if [ "$i" == "--all" ]            ; then install_dependencies         && \
-					     install_rtklib	          && \
+					     install_rtklib               && \
 					     install_rtkbase_from_release && \
-					     rtkbase_requirements	  && \
+					     rtkbase_requirements         && \
 					     install_unit_files           && \
-					     add_crontab		  && \
+					     add_crontab                  && \
 					     detect_usb_gnss              && \
 					     flash_gnss                      ;fi
 
