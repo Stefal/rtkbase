83f4a2e70ec54f41c312ccfdd44b0f0eb8549c9f
Stefal <stephane.peneau@wanadoo.fr>
2020-07-20 22:22:44 +0200

    moving shopt outside of the function Add function to insert "-tadj=1" during upgrade from v2.0.2

--- rtkbase_update.sh
+++ rtkbase_update.sh
@@ -7,6 +7,11 @@ data_dir=$3
 old_version=$4
 standard_user=$5
 
+#'shopt -s extglob' is needed for using (!pattern) exclusion pattern
+#from inside a script
+shopt -s extglob
+
+update() {
 echo "remove existing rtkbase.old directory"
 rm -rf /var/tmp/rtkbase.old
 mkdir /var/tmp/rtkbase.old
@@ -14,7 +19,6 @@ mkdir /var/tmp/rtkbase.old
 echo "copy rtkbase to rtkbase.old except /data directory"
 #'shopt -s extglob' is needed for using (!pattern) exclusion pattern
 #from inside a script
-shopt -s extglob
 cp -r ${destination_directory}/!(${data_dir}) /var/tmp/rtkbase.old
 
 echo "copy new release to destination"
@@ -22,6 +26,20 @@ cp -rfp ${source_directory}/. ${destination_directory}
 
 echo "delete the line version= in settings.conf"
 sed -i '/version=/d' ${destination_directory}/settings.conf
+}
+
+upd_2.0.2() {
+  if [[ $(grep -E "^receiver_format='ubx'" ${destination_directory}/settings.conf) ]]
+  then
+    # Add -TAJD=1 option to rtcm/ntrip output for ublox receivers
+    sed -i "/^rtcm_msg=.*/a ntrip_receiver_options='-TADJ=1'" ${destination_directory}/settings.conf
+    sed -i "/^rtcm_svr_msg=.*/a rtcm_receiver_options='-TADJ=1'" ${destination_directory}/settings.conf
+  fi
+  #upd_2.0.4
+}
+
+update
+upd_${old_version}
 
 #change rtkbase's content owner
 chown -R ${standard_user}:${standard_user} ${destination_directory}
