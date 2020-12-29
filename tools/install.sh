ca4cd7205c1417229af4d5b8d8455025bd319ecd
Stefal <stephane.peneau@wanadoo.fr>
2020-12-29 16:29:57 +0100

    Local prebuilt wheels for pystemd are not needed anymore.

--- tools/install.sh
+++ tools/install.sh
@@ -233,20 +233,12 @@ rtkbase_requirements(){
     echo '################################'
       #as we need to run the web server as root, we need to install the requirements with
       #the same user
-      # We are waiting for the next pystemd official release.
       # In the meantime, we install pystemd dev wheel for armv7 platform
       platform=$(uname -m)
-      if [[ $platform =~ 'armv7' ]]
+      if [[ $platform =~ 'aarch64' ]]
       then
-        python3 -m pip install ${rtkbase_path}/tools/pystemd-0.8.1590398158-cp37-cp37m-linux_armv7l.whl
-      elif [[ $platform =~ 'aarch64' ]]
-      then
-        python3 -m pip install ${rtkbase_path}/tools/pystemd-0.8.1599238382-cp37-cp37m-linux_aarch64.whl
         # More dependencies needed for aarch64 as there is no prebuilt wheel on piwheels.org
         apt-get install -y libssl-dev libffi-dev
-      elif [[ $platform =~ 'armv6' ]]
-      then
-        python3 -m pip install ${rtkbase_path}/tools/pystemd-0.8.1599232456-cp37-cp37m-linux_armv6l.whl
       fi
       python3 -m pip install --upgrade pip setuptools wheel  --extra-index-url https://www.piwheels.org/simple
       python3 -m pip install -r ${rtkbase_path}/web_app/requirements.txt  --extra-index-url https://www.piwheels.org/simple
