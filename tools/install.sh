694e981f12def2da39173f7d4042f42ac270130c
Stefal <stephane.peneau@wanadoo.fr>
2021-01-03 15:15:37 +0100

    Fixing link to rtklib apps

--- tools/install.sh
+++ tools/install.sh
@@ -136,12 +136,12 @@ install_rtklib() {
         sudo -u $(logname) wget -qO - https://github.com/tomojitakasu/RTKLIB/archive/v2.4.3-b34.tar.gz | tar -xvz
         #Install Rtklib app
         #TODO add correct CTARGET in makefile?
-        make --directory=RTKLIB-2.4.3-b34/app/consapp/str2str/gcc
-        make --directory=RTKLIB-2.4.3-b34/app/consapp/str2str/gcc install
-        make --directory=RTKLIB-2.4.3-b34/app/consapp/rtkrcv/gcc
-        make --directory=RTKLIB-2.4.3-b34/app/consapp/rtkrcv/gcc install
-        make --directory=RTKLIB-2.4.3-b34/app/consapp/convbin/gcc
-        make --directory=RTKLIB-2.4.3-b34/app/consapp/convbin/gcc install
+        make --directory=RTKLIB-2.4.3-b34/app/str2str/gcc
+        make --directory=RTKLIB-2.4.3-b34/app/str2str/gcc install
+        make --directory=RTKLIB-2.4.3-b34/app/rtkrcv/gcc
+        make --directory=RTKLIB-2.4.3-b34/app/rtkrcv/gcc install
+        make --directory=RTKLIB-2.4.3-b34/app/convbin/gcc
+        make --directory=RTKLIB-2.4.3-b34/app/convbin/gcc install
         #deleting RTKLIB
         rm -rf RTKLIB-2.4.3-b34/
       else
