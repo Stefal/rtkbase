1e747d1493c84de83e937c88012a416e88a00a16
Stefal <stephane.peneau@wanadoo.fr>
2021-01-05 16:46:32 +0100

    Adding rtkbase release, receiver model and antenna info to the rtcm streams

--- run_cast.sh
+++ run_cast.sh
@@ -11,7 +11,7 @@ source <( grep = ${BASEDIR}/settings.conf )  #import settings
 in_serial="serial://${com_port}:${com_port_settings}#${receiver_format}"
 in_tcp="tcpcli://127.0.0.1:${tcp_port}#${receiver_format}"
 
-out_caster="ntrips://:${svr_pwd}@${svr_addr}:${svr_port}/${mnt_name}#rtcm3 -msg ${rtcm_msg} -p ${position} -i ${receiver}"
+out_caster="ntrips://:${svr_pwd}@${svr_addr}:${svr_port}/${mnt_name}#rtcm3 -msg ${rtcm_msg} -p ${position} -i RTKBase_v${version}_${receiver} -a ${antenna_info}"
 #add receiver options if it exists
 [[ ! -z "${ntrip_receiver_options}" ]] && out_caster=""${out_caster}" -opt "${ntrip_receiver_options}""
 
