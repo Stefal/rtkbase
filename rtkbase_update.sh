c4ee6ab86cd43f48f8725285727fd9d5ba936c63
Stefal <stephane.peneau@wanadoo.fr>
2021-01-07 12:36:36 +0100

    Inserting 1008 and 1033 rtcm message during update

--- rtkbase_update.sh
+++ rtkbase_update.sh
@@ -24,6 +24,36 @@ cp -rfp ${source_directory}/. ${destination_directory}
 
 }
 
+insert_rtcm_msg() {
+# inserting new message inside a rtcm message list
+# and "return" the new strings with the ${new_rtcm} global variable
+# it will try to insert it a the lowest possible position
+
+
+    local text_line=${1}
+    local msg_to_insert=${2}
+    local highest_msg=${3}
+    local delay=${4}
+    
+    new_rtcm=''
+    if [[ ! $(echo ${text_line} | grep ${msg_to_insert}) ]]
+    then
+        for (( i=${msg_to_insert}; i<=${highest_msg}; i++ ))
+            do
+                if [[ $(echo ${text_line} | grep $i) ]]
+                then
+                    echo 'insert '${msg_to_insert}' before '$i
+                    new_rtcm=$(echo ${text_line} | sed 's|'"${i}"'|'"${msg_to_insert}${delay}"',&|')
+                    #echo ${new_rtcm}
+                    break
+                fi
+            done
+    else
+        #msg already inside the string
+        return 1
+    fi
+ }
+
 upd_2.0.2() {
   if [[ $(grep -E "^receiver_format='ubx'" ${destination_directory}/settings.conf) ]]
   then
@@ -55,16 +85,29 @@ upd_2.1.1() {
   make --directory=RTKLIB-2.4.3-b34/app/consapp/convbin/gcc install
   #deleting RTKLIB
   rm -rf RTKLIB-2.4.3-b34/
+  
   #restarting str2str_tcp service
   systemctl start str2str_tcp
+  
   #update python module
   python3 -m pip install -r ${destination_directory}'/web_app/requirements.txt'
+  
   #copying new service
   file_path=${destination_directory}'/unit/str2str_rtcm_serial.service'
   file_name=$(basename ${file_path})
-    echo copying ${file_name}
-    sed -e 's|{script_path}|'"$(dirname "$(readlink -f "$0")")"'|' -e 's|{user}|'"$(logname)"'|' ${file_path} > /etc/systemd/system/${file_name}
-    systemctl daemon-reload
+  echo copying ${file_name}
+  sed -e 's|{script_path}|'"$(dirname "$(readlink -f "$0")")"'|' -e 's|{user}|'"$(logname)"'|' ${file_path} > /etc/systemd/system/${file_name}
+  systemctl daemon-reload
+
+  #inserting new rtcm message 1008 and 1033 inside rtcm_msg and rtcm_svr_msg
+  insert_rtcm_msg $(grep "^rtcm_msg=" ${destination_directory}/settings.conf) 1008 1042 '(10)' && \
+      sed -i '/^rtcm_msg=/c\'"${new_rtcm}" ${destination_directory}/settings.conf
+  insert_rtcm_msg $(grep "^rtcm_msg=" ${destination_directory}/settings.conf) 1033 1230 '(10)' && \
+      sed -i '/^rtcm_msg=/c\'"${new_rtcm}" ${destination_directory}/settings.conf
+  insert_rtcm_msg $(grep "^rtcm_svr_msg=" ${destination_directory}/settings.conf) 1008 1042 '(10)' && \
+      sed -i '/^rtcm_svr_msg=/c\'"${new_rtcm}" ${destination_directory}/settings.conf
+  insert_rtcm_msg $(grep "^rtcm_svr_msg=" ${destination_directory}/settings.conf) 1033 1230 '(10)' && \
+      sed -i '/^rtcm_svr_msg=/c\'"${new_rtcm}" ${destination_directory}/settings.conf
   
 }
 
