088af073618859b28ea527afd30dce0459a51f11
Stefal <stephane.peneau@wanadoo.fr>
2023-07-06 16:31:10 +0000

    Update

--- tools/detect_lte_modem.sh
+++ tools/detect_lte_modem.sh
@@ -2,33 +2,33 @@
 
 detect_usb_lte_simcom_modem() {
     echo '################################'
-    echo 'SIMCOM LTE MODEM DETECTION'
+    echo 'SIMCOM A76XX LTE MODEM DETECTION'
     echo '################################'
       #This function try to detect a simcom lte modem (A76XX serie) and write the port inside settings.conf
 for sysdevpath in $(find /sys/bus/usb/devices/usb*/ -name dev); do
-          ID_SERIAL=''
-          MINOR=''
+          ID_MODEL=''
           syspath="${sysdevpath%/dev}"
           devname="$(udevadm info -q name -p "${syspath}")"
           if [[ "$devname" == "bus/"* ]]; then continue; fi
           eval "$(udevadm info -q property --export -p "${syspath}")"
-          if [[ $MINOR != 1 ]]; then continue; fi
-          if [[ -z "$ID_SERIAL" ]]; then continue; fi
-          if [[ "$ID_SERIAL" =~ 'SIMCom' ]]
+          #if [[ $MINOR != 1 ]]; then continue; fi
+          if [[ -z "$ID_MODEL" ]]; then continue; fi
+          if [[ "$ID_MODEL" =~ 'A76XX' ]]
           then
             detected_modem[0]=$devname
             detected_modem[1]=$ID_SERIAL
             echo '/dev/'"${detected_modem[0]}" ' - ' "${detected_modem[1]}"
+            #return 0
           fi
       done
+      return $?
     }
 
 detect_with_lsusb() {
           vendor_and_product_ids=$(lsusb | grep -i "A76XX" | grep -Eo "[0-9A-Za-z]+:[0-9A-Za-z]+")
           #echo 'vendor and product ids: ' "${vendor_and_product_ids}"
           if [[ -z "$vendor_and_product_ids" ]]; then 
-            echo 'NO GNSS RECEIVER DETECTED'
-            echo 'YOU CAN REDETECT IT FROM THE WEB UI'
+            echo 'NO LTE MODEM DETECTED'
             return 1
           fi
           devname=$(_get_device_path "$vendor_and_product_ids")
@@ -53,5 +53,4 @@ _get_device_path() {
     done
 }
 
-#detect_usb_lte_simcom_modem
-detect_with_lsusb
+detect_usb_lte_simcom_modem
