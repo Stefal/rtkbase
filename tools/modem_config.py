e8669291a62ccd751aa2919b41d663c071d65914
Stefal <stephane.peneau@wanadoo.fr>
2023-07-13 15:30:13 +0000

    update

--- tools/modem_config.py
+++ tools/modem_config.py
@@ -1,9 +1,9 @@
 #! /usr/bin/env python3
 
 import sys
-sys.path.insert(1, '/home/basegnss/sim-modem/src/')
+sys.path.insert(1, '/home/basegnss/sim-modem/')
 import argparse
-from sim_modem import *
+from src.sim_modem import *
 
 def arg_parse():
     parser=argparse.ArgumentParser(
@@ -61,7 +61,13 @@ if __name__ == '__main__':
         print("Data network registration", modem.get_eps_network_registration_status())
         print("Network mode selection: ", modem.get_network_mode())
         print("Current network mode: ", modem.get_current_network_mode().name)
-        print("EU system information: ",modem.get_eu_system_informations())
+        print("Current network name/operator: ", modem.get_network_operator())
+        print("EU system information: ", modem.get_eu_system_informations())
+        data_mode = modem.get_data_connection_mode()
+        if data_mode.name == 'ECM':
+            print("Modem is in ECM mode -> OK")
+        else:
+            print("Warning, the modem isn't in ECM mode but in {}".format(data_mode.name))
 
     if args.config:
         print("Configuring the modem in ECM Mode")
