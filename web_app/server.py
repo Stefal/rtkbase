d5b0106cdf7a3668472514a49e72c9feab1551fe
Stefal <stephane.peneau@wanadoo.fr>
2023-10-30 21:54:50 +0100

    fix url_parse deprecated in Werkzeug 3

--- web_app/server.py
+++ web_app/server.py
@@ -71,8 +71,8 @@ import distro
 
 from werkzeug.security import generate_password_hash
 from werkzeug.security import check_password_hash
-from werkzeug.urls import url_parse
 from werkzeug.utils import safe_join
+import urllib
 
 app = Flask(__name__)
 app.debug = False
@@ -430,7 +430,7 @@ def login_page():
         
         login_user(user, remember=loginform.remember_me.data)
         next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
+        if not next_page or urllib.parse.urlsplit(next_page).netloc != '':
             next_page = url_for('status_page')
 
         return redirect(next_page)
