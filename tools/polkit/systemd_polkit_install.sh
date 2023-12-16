dfad0f4eaf3efbd914a13f367f785567638581b7
Stefal <stephane.peneau@wanadoo.fr>
2023-12-16 19:31:05 +0100

    test username variable, and other fixes

--- tools/polkit/systemd_polkit_install.sh
+++ tools/polkit/systemd_polkit_install.sh
@@ -2,10 +2,11 @@
 #script to install the policykit rules which let manage the rtkbase services without root.
 #the user needs to be member of rtkbase group
 RTKBASE_USER=$1
-
-apt install policykit-1
-mkdir /etc/polkit-1/rules.d/
-cp ./*.rules /etc/polkit-1/rules.d/
+[[ -z "${RTKBASE_USER}" ]] && echo 'Please specify a username' && exit 1
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+apt-get install -y policykit-1
+mkdir -p /etc/polkit-1/rules.d/
+cp "${SCRIPT_DIR}"/*.rules /etc/polkit-1/rules.d/
 
 groupadd rtkbase
 usermod -a -G rtkbase "${RTKBASE_USER}"
