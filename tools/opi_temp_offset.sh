a333abdfc694b66e6baa740a8dcf5cdf62646a2b
Stefal <stephane.peneau@wanadoo.fr>
2023-12-29 14:38:55 +0100

    During installation, cpu temps could be higher than 20°C. Raising up the threshold to 30°C

--- tools/opi_temp_offset.sh
+++ tools/opi_temp_offset.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 detect_sbc_temp_offset(){
-  #Detect if the computer is a Orange Pi Zero, and if the cpu temps is lower than 20°C, it looks like it's a Orange Pi zero LTS with a 30°C offset
+  #Detect if the computer is a Orange Pi Zero, and if the cpu temps is lower than 30°C, it looks like it's a Orange Pi zero LTS with a 30°C offset
   computer_model=$(tr -d '\0' < /sys/firmware/devicetree/base/model)
   cpu_temp=$(($(cat /sys/class/thermal/thermal_zone0/temp)/1000))
-  if [[ $computer_model = 'Xunlong Orange Pi Zero' ]] && [[ cpu_temp -lt 20 ]]
+  if [[ $computer_model = 'Xunlong Orange Pi Zero' ]] && [[ cpu_temp -lt 30 ]]
   then
     echo 'Adding 30°C offset'
     if [[ -f "${rtkbase_path}/settings.conf" ]]  && grep -qE "^cpu_temp_offset=.*" "${rtkbase_path}"/settings.conf #check if settings.conf exists
