2c9736e4ffef314a3621ab282db6bf5e307a8f65
Stefal <stephane.peneau@wanadoo.fr>
2024-01-15 20:30:05 +0100

    quietting grep command

--- tools/install_polkit_rules.sh
+++ tools/install_polkit_rules.sh
@@ -13,7 +13,7 @@ add_polkit_rules() {
 }
 
 #check if polkitd package is available, else exit
-apt-cache --quiet=0 show polkitd 2>&1 | grep 'No packages found' && exit 1
+apt-cache --quiet=0 show polkitd 2>&1 | grep -q 'No packages found' && exit 1
 #install it if not already installed
 ! dpkg-query -W --showformat='${Status}\n' polkitd >/dev/null 2>&1 && apt-get -y install polkitd
 add_polkit_rules
