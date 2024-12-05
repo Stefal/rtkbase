348813c2d9a066236342de8066d2a231d09f131c
Stefal <stephane.peneau@wanadoo.fr>
2024-12-05 08:57:14 +0000

    Small fixes

--- tools/sept_tool.py
+++ tools/sept_tool.py
@@ -8,9 +8,8 @@ from enum import Enum
 from operator import methodcaller
 
 class CmdMapping(Enum):
-    """Mapping human command to septentrio methods"""
+    """Mapping human command to septentrio_cmd methods"""
 
-    #get_model = methodcaller('get_receiver_model')
     get_model = 'get_receiver_model'
     get_firmware = 'get_receiver_firmware'
     get_ip = 'get_receiver_ip'
@@ -21,36 +20,38 @@ def arg_parse():
     """ Parse the command line you use to launch the script """
     
     parser= argparse.ArgumentParser(prog='Septentrio tool', description="A tool to send comment to a Septentrio GNSS receiver")
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument("-p", "--port", help="Port to connect to", type=str)
     parser.add_argument("-b", "--baudrate", help="port baudrate", default=115200, type=int)
-    parser.add_argument("-c", "--command", nargs='+', help="Command to send to the gnss receiver", type=str)
+    parser.add_argument("-c", "--command", nargs='+', help="Command to send to the gnss receiver.\nAvailable commands are: 'get_model' 'get_firmware' 'reset' 'send_config_file'", type=str)
     parser.add_argument("-s", "--store", action='store_true', help="Store settings as permanent", default=False)
     parser.add_argument("-r", "--retry", help="set a number of retry if the command fails", default=0, type=int)
-    parser.add_argument("--version", action="version", version="%(prog)s 1.0")
+    parser.add_argument("-d", "--debug", action='store_true')
+    parser.add_argument("--version", action="version", version="%(prog)s 1.1")
     args = parser.parse_args()
-    #print(args)
     return args
 
 if __name__ == '__main__':
     args = arg_parse()
-    #print(args)
+    if args.debug:
+        print(args)
     command = args.command[0]
     retries = 0
     retry_delay = 10
     while retries <= args.retry:
         try:
-            with SeptGnss(args.port, baudrate=args.baudrate, timeout=30, debug=False) as gnss:
+            with SeptGnss(args.port, baudrate=args.baudrate, timeout=30, debug=args.debug) as gnss:
                 res = methodcaller(CmdMapping[command].value, *args.command[1:])(gnss)
                 if type(res) is str:
                     print(res)
                 if args.store:
                     gnss.set_config_permanent()
-                #methodcaller(args.command[0])(gnss)
             break
         except:
             retries += 1
-            print("Failed...retrying in {}s".format(retry_delay))
-            time.sleep(retry_delay)
+            if retries <= args.retry:
+                print("Failed...retrying in {}s".format(retry_delay))
+                time.sleep(retry_delay)
     if retries > args.retry:
         print("Command failed!")
         sys.exit(1)
