c4cb52a65cea958186fcb56283c40dce8ce194f0
Stefal <stephane.peneau@wanadoo.fr>
2024-12-17 21:52:28 +0100

    replace send_file to send_from_directory fix path traversal vulnerability

--- web_app/server.py
+++ web_app/server.py
@@ -53,7 +53,7 @@ from RTKBaseConfigManager import RTKBaseConfigManager
 
 from flask_bootstrap import Bootstrap4
 from flask import Flask, render_template, session, request, flash, url_for
-from flask import send_file, send_from_directory, redirect, abort
+from flask import send_from_directory, redirect, abort
 from flask import g
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, BooleanField, SubmitField
@@ -465,8 +465,7 @@ def logs_page():
 def downloadLog(log_name):
     """ Route for downloading raw gnss data"""
     try:
-        full_log_path = rtk.logm.log_path + "/" + log_name
-        return send_file(full_log_path, as_attachment = True)
+        return send_from_directory(rtk.logm.log_path, log_name, as_attachment = True)
     except FileNotFoundError:
         abort(404)
 
@@ -670,7 +669,9 @@ def reset_settings():
 @login_required
 def backup_settings():
     settings_file_name = str("RTKBase_{}_{}_{}.conf".format(rtkbaseconfig.get("general", "version"), rtkbaseconfig.get("ntrip_A", "mnt_name_a").strip("'"), time.strftime("%Y-%m-%d_%HH%M")))
-    return send_file(os.path.join(rtkbase_path, "settings.conf"), as_attachment=True, download_name=settings_file_name)
+    #return send_file(os.path.join(rtkbase_path, "settings.conf"), as_attachment=True, download_name=settings_file_name)
+    return send_from_directory(rtkbase_path, "settings.conf", as_attachment=True, download_name=settings_file_name)
+
 
 @socketio.on("restore settings", namespace="/test")
 def restore_settings_file(json_msg):
