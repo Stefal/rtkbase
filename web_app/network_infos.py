92a740b3089440afaea8dbf31c66d1259bcf9ecd
Stefal <stephane.peneau@wanadoo.fr>
2024-12-24 00:18:43 +0100

    keep wireguard vpn link

--- web_app/network_infos.py
+++ web_app/network_infos.py
@@ -42,8 +42,9 @@ def get_up_if():
     #filtering available interface
     if_stats = psutil.net_if_stats()
     if_stats.pop('lo')
+    log.debug(if_stats)
     if_stats = {k:v for (k,v) in if_stats.items() if if_stats[k].isup} # keep only up interface
-    if_stats = {k:v for (k,v) in if_stats.items() if if_stats[k].speed > 0} # keep only interface with speed > 0
+    if_stats = {k:v for (k,v) in if_stats.items() if if_stats[k].speed > 0 or 'pointopoint' in if_stats[k].flags} # keep only interface with speed > 0
     if_stats = {k:v for (k,v) in if_stats.items() if not k.startswith('docker')} # remove docker interface
     return if_stats
 
@@ -61,7 +62,7 @@ def get_interfaces_infos():
     log.debug("up interfaces: {}".format(up_interface))
     #then filter psutil.net_if_addrs()
     if_addrs = psutil.net_if_addrs()
-    up_interface = {k:if_addrs[k] for (k,v) in up_interface.items() if if_addrs[k]}
+    up_interface = {k:if_addrs[k] for (k,v) in up_interface.items() if if_addrs.get(k)}
     #then filter to keep only AF_INET and AF_INET6
     for k,v in up_interface.items():
         up_interface[k] = [ x for x in v if x.family.name == 'AF_INET' or x.family.name == 'AF_INET6']
